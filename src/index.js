a3fdad4169a398474aee720d10b7f02b30e3c2fa
Jerry Tarus <jerrytarus@gmail.com>
2023-10-19 15:13:09 +0300

    Cross checked functionality

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
+
+
     // hide & seek with the form
+    
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
@@ -64,6 +67,52 @@ function likes(e) {
       e.target.previousElementSibling.innerText = `${more} likes`;
     }))
 }
+
+
+function renderToys(toy) {
+  let h2 = document.createElement('h2')
+  h2.innerText = toy.name
+  let img = document.createElement('img')
+  img.setAttribute('src', toy.image)
+  img.setAttribute('class', 'toy-avatar')
+  let p = document.createElement('p')
+  p.innerText = `${toy.likes} likes`
+  let btn = document.createElement('button')
+  btn.setAttribute('class', 'like-btn')
+  btn.setAttribute('id', toy.id)
+  btn.innerText = "like"
+  btn.addEventListener('click', (e) => {
+    console.log(e.target.dataset);
+    likes(e)
+  })
+  let divCard = document.createElement('div')
+  divCard.setAttribute('class', 'card')
+  divCard.append(h2, img, p, btn)
+  divCollect.append(divCard)
+}
+// Adding an event listener to 'Add Toy' button to show or hide form
+addBtn.addEventListener('click', () => {
+
+  // hide & seek with the form
+
+  addToy = !addToy
+  if (addToy) {
+    toyForm.style.display = 'block'
+    toyForm.addEventListener('submit', event => {
+      event.preventDefault()
+      postToy(event.target)
+    })
+  } else {
+    toyForm.style.display = 'none'
+  }
+})
+getToys().then(toys => {
+  toys.forEach(toy => {
+
+    //function to render toys goes here
+    renderToys(toy)
+  })
+})
 
 
 
